a84cac2cce713091729d2351ea624b789c3229a3
VitaPetrushyna <masandra2018@ukr.net>
2022-09-29 21:59:58 +0300

    task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countOwnFeedback = event => {
+    const name = event.target.name;
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  };
+
+  // countPositiveFeedbackPercentage()
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <h1>Please leave feedback</h1>
+        <ul>
+          <li>
+            <button type="button" onClick={this.countOwnFeedback}>
+              Good
+            </button>
+          </li>
+          <li>
+            <button type="button" onClick={this.countOwnFeedback}>
+              Neutral
+            </button>
+          </li>
+          <li>
+            <button type="button" onClick={this.countOwnFeedback}>
+              Bad
+            </button>
+          </li>
+        </ul>
+        <h2>Statistics</h2>
+        <ul>
+          <li>Good: {good} </li>
+          <li>Neutral: {neutral} </li>
+          <li>Bad: {bad} </li>
+          <li>Total: {this.total}</li>
+        </ul>
+      </>
+    );
+  }
+}
