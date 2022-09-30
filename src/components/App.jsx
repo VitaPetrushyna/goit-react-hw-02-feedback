bdecc10f70e78460131c8e0de8f52b68e88c0820
VitaPetrushyna <masandra2018@ukr.net>
2022-09-30 21:30:15 +0300

    task 1 1-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,11 @@
 import { Component } from 'react';
+import {
+  Feedback,
+  FeedbackList,
+  FeedbackBtn,
+  Statistic,
+  StatisticList,
+} from './App.styled';
 
 export class App extends Component {
   state = {
@@ -7,51 +14,62 @@ export class App extends Component {
     bad: 0,
   };
 
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const result = good + neutral + bad;
+    return result;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good * 100) / this.countTotalFeedback());
+  };
+
   countOwnFeedback = event => {
-    const name = event.target.name;
+    const label = event.target.textContent;
     this.setState(prevState => ({
-      [name]: prevState[name] + 1,
+      [label]: prevState[label] + 1,
     }));
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    return total;
-  };
-
-  // countPositiveFeedbackPercentage()
-
   render() {
     const { good, neutral, bad } = this.state;
+
+    const total = this.countTotalFeedback();
+    const countPercentage = this.countPositiveFeedbackPercentage();
     return (
       <>
-        <h1>Please leave feedback</h1>
-        <ul>
-          <li>
-            <button type="button" onClick={this.countOwnFeedback}>
-              Good
-            </button>
-          </li>
-          <li>
-            <button type="button" onClick={this.countOwnFeedback}>
-              Neutral
-            </button>
-          </li>
-          <li>
-            <button type="button" onClick={this.countOwnFeedback}>
-              Bad
-            </button>
-          </li>
-        </ul>
-        <h2>Statistics</h2>
-        <ul>
-          <li>Good: {good} </li>
-          <li>Neutral: {neutral} </li>
-          <li>Bad: {bad} </li>
-          <li>Total: {this.total}</li>
-        </ul>
+        <Feedback>
+          <h1>Please leave feedback</h1>
+          <FeedbackList>
+            {Object.keys(this.state).map(option => {
+              return (
+                <li key={option}>
+                  <FeedbackBtn type="button" onClick={this.countOwnFeedback}>
+                    {option}
+                  </FeedbackBtn>
+                </li>
+              );
+            })}
+          </FeedbackList>
+        </Feedback>
+        <Statistic>
+          <h2>Statistics</h2>
+          <StatisticList>
+            <li>Good: {good} </li>
+            <li>Neutral: {neutral} </li>
+            <li>Bad: {bad} </li>
+            <li>Total: {total}</li>
+            <li>PositivePercentage: {countPercentage}%</li>
+          </StatisticList>
+        </Statistic>
       </>
     );
   }
 }
+
+// countOwnFeedback = event => {
+//   // const name = event.target.name;
+//   this.setState(prevState => ({
+//     good: prevState.good + 1,
+//   }));
+// };
